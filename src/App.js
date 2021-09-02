d55561d474e5c8d2a77a69c00181591ad20bd159
ConstanceNwaigwe <constancenwaigwe@gmail.com>
2021-09-01 23:27:49 -0400

    added css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Todo from './components/Todo';
 import TodoList from './components/TodoList';
 import TodoForm from './components/TodoForm';
+import './components/Todo.css';
 
 const thingsTodo = [
   {
